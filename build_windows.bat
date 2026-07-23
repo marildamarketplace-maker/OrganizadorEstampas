b211e78f203faf1682185f70d4eb0624553aab45
Marco Túlio <marcotulio@MacBook-Air-de-Marco.local>
2026-07-23 18:56:41 -0300

    ajustes

--- build_windows.bat
+++ build_windows.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 if not exist .venv (
-    python -m venv .venv
+    py -m venv .venv
 )
 call .venv\Scripts\activate
-python -m pip install --upgrade pip
+py -m pip install --upgrade pip
 pip install -r requirements.txt
 pyinstaller --noconfirm --clean --windowed --name "OrganizadorEstampasMeury" app.py
 echo.
